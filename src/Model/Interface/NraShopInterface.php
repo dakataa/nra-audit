328977214078793d96a923f275bd3aa6c007a5b0
Yordan Lazarov <yordanlazarov@Dakata-765.local>
2025-10-10 09:51:10 +0300

    Add more information about the store

--- src/Model/Interface/NraShopInterface.php
+++ src/Model/Interface/NraShopInterface.php
@@ -6,13 +6,22 @@ use Dakataa\NraAudit\Enum\ShopTypeEnum;
 
 interface NraShopInterface
 {
+	public function getNraShopName(): string;
 
 	public function getNraShopEik(): string;
+
+	public function getNraShopMOL(): string;
 
 	public function getNraShopNumber(): string;
 
 	public function getNraShopDomain(): string;
 
 	public function getNraShopType(): ShopTypeEnum;
+
+	public function getNraShopEmail(): string;
+
+	public function getNraShopPhone(): ?string;
+
+	public function getNraShopAddress(): string;
 
 }
